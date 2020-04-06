869c49fd457a41a7068207050282add2c5b5b4b9
surya8barca <surya8barca@gmail.com>
2020-04-06 14:20:44 +0530

    updated main.dart

--- Android App/attendance_app/lib/main.dart
+++ Android App/attendance_app/lib/main.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
@@ -16,7 +14,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.blueGrey,
         centerTitle: true,
@@ -27,6 +25,68 @@ class _HomeState extends State<Home> {
             color: Colors.black,
           ),
         ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            color: Colors.green[800],
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Center(
+                      child: Text(
+                        'FR. CONCEICAO RODRIGUES COLLEGE OF ENGINEERING',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14.0,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        '(Approved by AICTE & Affiliated tp university of Mumbai)',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        'Bandstand,Bandra (W), Mumbai - 400 050. *Tel.: 6711 4000 * Fax: 6711 4200 ',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                  ),
+              ],
+            ),
+          ),
+          SizedBox(height: 15.0,),
+          Center(
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('http://gyan.fragnel.edu.in:2222/moodle/data/agnel.jpg'),
+              radius: 80.0,
+            ),
+          ),
+          SizedBox(height: 15.0,),
+          Divider(
+            height:5.0,
+            thickness:2.0,
+            color:Colors.blueAccent,
+            indent:30.0,
+            endIndent:30.0, 
+          ),
+          SizedBox(height: 15.0,),
+
+        ]
       ),
     );
   }
