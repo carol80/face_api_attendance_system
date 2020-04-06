a87b17f8c03640f4f2b9fba44db59e5eef3c1fc2
surya8barca <surya8barca@gmail.com>
2020-04-06 17:15:22 +0530

    finish homepage

--- Android App/attendance_app/lib/main.dart
+++ Android App/attendance_app/lib/main.dart
@@ -1,3 +1,6 @@
+//Intro Page
+
+import 'package:attendance_app/login.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
@@ -85,8 +88,84 @@ class _HomeState extends State<Home> {
             endIndent:30.0, 
           ),
           SizedBox(height: 15.0,),
-
-        ]
+          Center(
+            child: Text(
+              'Attendance System Using',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                decoration: TextDecoration.underline,
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              'Face Detection',
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+                decoration: TextDecoration.underline,
+              ),
+            ),
+          ),
+          SizedBox(height: 15.0,),
+          Divider(
+            height:5.0,
+            thickness:2.0,
+            color:Colors.blueAccent,
+            indent:30.0,
+            endIndent:30.0, 
+          ),
+          SizedBox(height: 50.0,),
+          Center(
+            child: ButtonTheme(
+                minWidth: 150,
+                height: 60,
+                buttonColor: Colors.lightBlueAccent,
+                child: RaisedButton(
+                onPressed: (){
+                  Route route = MaterialPageRoute(builder: (context) => Login());
+                  Navigator.push(context, route);
+                }, 
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30.0,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Text(
+                    '*Only for teachers',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 10.0,
+                    ),
+                  ),
+            ),
+          ),
+          SizedBox(height: 50.0),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                    'Developed By: Carol Sebastian, Surya Partap and Kevin Ruffin (TE Comps)',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 10.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+            ),
+          ),
+       ]
       ),
     );
   }
